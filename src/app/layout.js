a975a3906df502d443de207303ebc678df299215
irfan-za <fanzahran123@gmail.com>
2023-11-21 21:05:09 +0700

    fix: update logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
   description: 'Sumber Griya Abadi merupakan perusahaan jasa pemasangan plafon dan distribusi bahan material bangunan. Kami berdiri sejak 2008 di Solo, Jawa Tengah. Hingga saat ini, kami selalu konsisten melayani pemasangan plafon secara professional dan berkualitas.  ',
   keywords: 'toko plafon, pemborong plafon, jasa konstruksi, jasa pasang plafon, toko bangunan, plafon pvc, plafon gypsum, baja ringan, atap galvalume, pasang plafon pvc, tukang plafon, atap rumah, pemasangan plafom pvc, toko plafon solo, toko bangunan solo',
   openGraph: {
-    images: '/logo.png',
+    images: 'https://res.cloudinary.com/ddjmxrrij/image/upload/v1700575460/logo_rcth5s.png',
   },
   verification: {
     google: 'M7smp6GOF5tNu2OC4jm7WhbeuzyeL2GQM-_tcjOe_xo',
