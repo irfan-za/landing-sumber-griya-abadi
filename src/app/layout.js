bcb293f6b5de6d05ba4532b3904babac1dbb584a
Muhammad Irfan Zahran <fanzahran123@gmail.com>
2023-12-02 14:25:16 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Sumber Griya Abadi | Jasa Pemasangan Plafon | Toko Plafon di Solo',
+  title: 'Jasa Pemasangan Plafon PVC & Gypsum | Toko Plafon di Solo',
   description: 'Jasa pemasangan plafon Gypsum, PVC dan Baja Ringan, jual berbagai bahan material bangunan lengkap hanya di Sumber Griya Abadi.',
   keywords: 'toko plafon, pemborong plafon, jasa konstruksi, jasa pasang plafon, toko bangunan, plafon pvc, plafon gypsum, baja ringan, atap galvalume, pasang plafon pvc, tukang plafon, atap rumah, pemasangan plafom pvc, toko plafon solo, toko bangunan solo',
   openGraph: {
