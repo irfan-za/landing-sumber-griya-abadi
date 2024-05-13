640bb439533c20357febfbeabd58d2ee40f42351
irfan-za <fanzahran123@gmail.com>
2024-05-13 09:00:55 +0700

    feat: add google analytics tracking

--- src/app/p/layout.js
+++ src/app/p/layout.js
@@ -2,6 +2,7 @@ import ButtonWA from "@/components/ButtonWA";
 import "@/app/globals.css";
 import { Inter } from "next/font/google";
 import FacebookPixel from "@/components/product/FacebookPixel";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function ProductsLayout({ children }) {
@@ -12,6 +13,19 @@ export default function ProductsLayout({ children }) {
         <FacebookPixel />
         <ButtonWA />
       </body>
+      <Script
+        strategy="lazyOnLoad"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+      ></Script>
+      <Script id="ga-script" strategy="lazyOnLoad">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-Z1EP20ZRED');
+        `}
+      </Script>
     </html>
   );
 }
