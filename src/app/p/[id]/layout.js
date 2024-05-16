db9a895ea97dc0f671df93627812e4c2f7c61daf
irfan-za <fanzahran123@gmail.com>
2024-05-16 22:39:58 +0700

    fix: google analytic id

--- src/app/p/[id]/layout.js
+++ src/app/p/[id]/layout.js
@@ -2,9 +2,9 @@ import ButtonWA from "@/components/ButtonWA";
 import "@/app/globals.css";
 import { Inter } from "next/font/google";
 import FacebookPixel from "@/components/product/FacebookPixel";
-import Script from "next/script";
 import { getItem } from "@/lib/utils/supabaseCRUD";
 import { notFound } from "next/navigation";
+import { GoogleAnalytics } from "@next/third-parties/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export async function generateMetadata({ params }) {
@@ -28,19 +28,7 @@ export default function ProductLayout({ children }) {
         <FacebookPixel />
         <ButtonWA />
       </body>
-      <Script
-        strategy="lazyOnLoad"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-      ></Script>
-      <Script id="ga-script" strategy="lazyOnLoad">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID});
-        `}
-      </Script>
+      <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID} />
     </html>
   );
 }
