f95786a8634494855db9013fcb793e571e1ed66f
irfan-za <fanzahran123@gmail.com>
2025-06-01 22:42:33 +0700

    feat: add blogs to sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -35,6 +35,17 @@ export default async function sitemap() {
       priority: 0.8,
     };
   });
+
+  const { data: blogs, error: blogError } = await getAll("blogs");
+  const blogs_url = blogs.map((blog) => {
+    return {
+      url: `${process.env.NEXT_PUBLIC_WEBSITE_URL}/blogs/${blog.id}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.6,
+    };
+  });
+
   return [
     {
       url: `${process.env.NEXT_PUBLIC_WEBSITE_URL}`,
@@ -57,5 +68,6 @@ export default async function sitemap() {
     ...offline_products_url,
     ...services_url,
     ...products_url,
+    ...blogs_url,
   ];
 }
